ba72ba6ad11649a4d10765431d65d343aa68d5d3
Ikko Eltociear Ashimine <eltociear@gmail.com>
2023-12-25 00:15:40 +0900

    Update eval_seg.py
    Evalute -> Evaluate

--- scripts/eval_seg.py
+++ scripts/eval_seg.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------
-"""Evalute bare segmentation using detection boxes."""
+"""Evaluate bare segmentation using detection boxes."""
 
 import argparse
 import collections
